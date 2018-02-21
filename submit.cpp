982c923d3043735440a591b256143d58794074f8
UCSB Training Admin <ucsb-train41@tscc-login2.sdsc.edu>
2018-02-20 21:28:19 -0800

    copy the harness

---
+++ submit.cpp
@@ -0,0 +1,18 @@
+#include "innerproduct.h"
+
+double rec_cilkified(double *a, double *b, int n)
+{
+	
+	return 1;
+}
+
+double loop_cilkified(double *a, double *b, int n)
+{
+	return 360;
+}
+
+double hyperobject_cilkified(double *a, double *b, int n)
+{
+	return 1;
+}
+
